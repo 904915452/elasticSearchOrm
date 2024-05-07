e21747393fbb36692a0c6c29658cf331ee5b12ec
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 <904915452@qq.com>
2024-05-07 16:19:15 +0800

    test

--- src/Builder.php
+++ src/Builder.php
@@ -3,8 +3,16 @@
  * @Author: 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 904915452@qq.com
  * @Date: 2024-05-07 15:57:24
  * @LastEditors: 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 904915452@qq.com
- * @LastEditTime: 2024-05-07 15:57:32
+ * @LastEditTime: 2024-05-07 16:18:21
  * @FilePath: \elasticSearchOrm\src\1.php
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 链式操作生成
  */
-$a = 111;
+
+namespace elasticssearchorm;
+
+class Builder
+{
+    public function test()
+    {
+    }
+}
