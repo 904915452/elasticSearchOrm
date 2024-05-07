caad8d3e7c9dfd3d7beab48d00bcb01947508ffa
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 <904915452@qq.com>
2024-05-07 17:04:45 +0800

    no message

--- src/Builder.php
+++ src/Builder.php
@@ -3,12 +3,12 @@
  * @Author: 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 904915452@qq.com
  * @Date: 2024-05-07 15:57:24
  * @LastEditors: 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 904915452@qq.com
- * @LastEditTime: 2024-05-07 16:18:21
+ * @LastEditTime: 2024-05-07 17:03:39
  * @FilePath: \elasticSearchOrm\src\1.php
- * @Description: 链式操作生成
+ * @Description: 链式操作生成 
  */
 
-namespace elasticssearchorm;
+namespace ElasticsSearchOrm;
 
 class Builder
 {
