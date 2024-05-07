819568eeeed4751aa6f6e7b818a8bc436ddf90cc
904915452 <904915452@qq.com>
2024-05-07 17:21:39 +0800

    1

--- src/Builder.php
+++ src/Builder.php
@@ -2,13 +2,13 @@
 /*
  * @Author: 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 904915452@qq.com
  * @Date: 2024-05-07 15:57:24
- * @LastEditors: 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 904915452@qq.com
- * @LastEditTime: 2024-05-07 17:03:39
+ * @LastEditors: 904915452 904915452@qq.com
+ * @LastEditTime: 2024-05-07 17:21:29
  * @FilePath: \elasticSearchOrm\src\1.php
  * @Description: 链式操作生成 
  */
 
-namespace ElasticsSearchOrm;
+namespace ElasticSearchOrm;
 
 class Builder
 {
